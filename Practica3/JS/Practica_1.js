d55942c4af908f48c8ffab0f0b4a498a3dfac776
franPzTo <franperezt1@gmail.com>
2025-08-18 21:26:45 -0300

    index y punto 1: terminado

---
+++ Practica3/JS/Practica_1.js
@@ -0,0 +1,22 @@
+let meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+]
+document.write("<div class=\"mx-5 text-danger\">");
+document.write("<h2>Lista de Meses</h2>");
+document.write("<ul>");
+for (let i = 0; i < meses.length; i++) {
+    document.write("<li>" + meses[i] + "</li>");
+}
+document.write("</ul></div>");
+document.write("");
